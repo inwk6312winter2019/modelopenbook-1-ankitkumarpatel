58bcc3625658e7b248c98e88aa8c5bbb8ac62e0a
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 08:00:06 -0800

    added framework for problem

---
+++ openbook1.py
@@ -0,0 +1,24 @@
+#
+
+
+
+
+
+def list_ifname_ip(filename):
+    
+    file = open(filename)
+    dic = dict()
+    for line in file:
+        line
+         dic[nameif]=(IPaddress,NetMask)
+return dic
+
+
+
+
+
+
+
+list_ifname_ip('running-config.cfg')
+
+
