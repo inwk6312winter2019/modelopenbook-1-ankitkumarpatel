92b1f30c7eefe6c287e46751d3b0e1063afd0481
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 11:46:19 -0800

    task 3 completed

--- openbook1.py
+++ openbook1.py
@@ -20,7 +20,8 @@ def list_ifname_ip(filename):
 	    	#print(ip)
                 dic.setdefault(name[1],(ip[2],ip[3]))
 
-    print(dic)        
+    #print(dic)
+    return(dic)        
 
 
 def new_config_file(filename):
@@ -45,7 +46,70 @@ def new_config_file(filename):
     fout.close()
 
 
-list_ifname_ip('running-config.cfg')
+
+
+def access_list(filename):
+    fin = open(filename,'r')
+    global_access=[]
+    transit_access_in=[]
+    fw_management_access_in=[]
+
+
+
+    for line in fin:
+        if 'global_access' in line:
+            lst1 = []
+            line = line.strip().split()
+	    for word in line:
+                if word[0].isupper():
+                    lst1.append(word)
+            global_access.extend([lst1])
+
+
+        if 'transit_access_in' in line:
+            lst2 = []
+            line = line.strip().split()
+	    for word in line:
+                if word[0].isupper():
+                    lst2.append(word)
+            transit_access_in.extend([lst2])
+
+
+	if 'fw-management_access_in' in line:
+            lst3 = []
+            line = line.strip().split()
+	    for word in line:
+                if word[0].isupper():
+                    lst3.append(word)
+            fw_management_access_in.extend([lst3])
+
+
+    print("access list for global_access:")
+    print(global_access)
+
+
+    print("\naccess list for transit_access_in:")
+    print(transit_access_in)
+
+
+    print("\naccess list for fw_management_access_in:")
+    print(fw_management_access_in)
+
+
+"""
+
+result = list_ifname_ip('running-config.cfg')
+
+print(result)
 
 new_config_file('running-config.cfg')
+
+
+"""
+access_list('running-config.cfg')
+
+
+
+
+
 
