3d4c67e6b22f5ef62905fe2e9140dae7fac8a96b
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 10:11:26 -0800

    tried to replace strings mentyioned in task 2

--- openbook1.py
+++ openbook1.py
@@ -6,25 +6,41 @@
 
 def list_ifname_ip(filename):
     
-    file = open(filename)
+    fin = open(filename)
     dic = dict()
-    for line in file:
+    for line in fin:
         if 'no' not in line: 
             if 'nameif' in line:
                 name = line.strip().split()
-	    	print(name)
+	    	#print(name)
 
 
 	    if 'ip address' in line:
 	        ip = line.strip().split()
-	    	print(ip)
+	    	#Sprint(ip)
                 dic.setdefault(name[1],(ip[2],ip[3]))
 
     print(dic)        
 
 
+def new_config_file(filename):
+    
+    fout = open("new_config.cfg","a")
+    fin = open(filename,"r")
+    for line in fin:
+        if '172.0' in line:
+            line = line.replace("172.", "10.")
+        if '192.0' in line:
+            line = line.replace("192.", "10.")
+        if '255.255.0.0' in line:
+            line = line.replace("255.255.0.0","255.0.0.0")
+        if '255.255.255.0' in line:
+            line = line.replace("255.255.255.0","255.0.0.0")
+        fout.write(line)
+    fout.close()
 
 
 list_ifname_ip('running-config.cfg')
 
+new_config_file('running-config.cfg')
 
