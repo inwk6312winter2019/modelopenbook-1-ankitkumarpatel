de9d58664171a99c6ca43bf11a49dec674d0a78c
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 09:44:27 -0800

    first part completed

--- openbook1.py
+++ openbook1.py
@@ -9,19 +9,18 @@ def list_ifname_ip(filename):
     file = open(filename)
     dic = dict()
     for line in file:
-        line = line.split()
-        for i in line:
-            if any(line[i] == 'nameif'):
-                nameif = line[i+1]
-            if any(line[i] == 'ip')
-                IPaddress = line[i+2]
-                NetMask = line[i+3]
-            if  
-            dic[nameif]=(IPaddress,NetMask)
-return dic
+        if 'no' not in line: 
+            if 'nameif' in line:
+                name = line.strip().split()
+	    	print(name)
 
 
+	    if 'ip address' in line:
+	        ip = line.strip().split()
+	    	print(ip)
+                dic.setdefault(name[1],(ip[2],ip[3]))
 
+    print(dic)        
 
 
 
